0c08784f722511a21f84e42c72c5c28dff9e7050
Aravind Parappil <aravindparappil@gmail.com>
2019-11-11 23:01:13 -0500

    Added FormGroup to get vals in registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
+   registerForm = new FormGroup({
+	    userName: new FormControl('', [
+	      Validators.required
+	    ]),
+	    email: new FormControl('', [
+	      Validators.required
+	      
+	    ]),
+	    pwd: new FormControl('', [
+	      Validators.required,
+	      Validators.minLength(8)
+	    ]),
+	    confirmPwd:new FormControl('', [
+	      Validators.required,
+	      Validators.minLength(8)
+	    ])
+  });
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+  	console.log(this.registerForm.value);
   }
 
 }
